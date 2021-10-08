e3813d2935b5246b6878fa01b8ddb3967244fdf2
Manuel Segimón <manuelsegimon68@gmail.com>
2021-10-07 20:21:46 -0400

    Implement callback fix

--- assets/respondToBoi.js
+++ assets/respondToBoi.js
@@ -1,56 +1,69 @@
-function isItQuestion(message) {
+function isItQuestion(message, questionCallback) {
     $.post("./ajax.php", { call: "isQuestion", message: message }, result => {
 
-        console.log(result);
+        questionCallback(result);
 
     });
 }
 
 function generateBasicResponse(message, callback) {
-    let isQuestion = isItQuestion(message);
+    
+    isItQuestion(message, result => {
+        let isQuestion = Number(result)
 
-    if (isQuestion == 1) {
-        
-        // Wolfram Alpha Api Inclusion
-        let question = message
-        while (question.includes(" ")) {
-            question = question.replace(" ", "+");
-        }
-        while (question.includes("?")) {
-            question = question.replace("?", "%3f")
-        }
+        console.log(isQuestion);
 
-        $.post("./ajax.php", {call: "wolframApiCall", question}, result => {
+        if (isQuestion == 1) {
 
-            let json = JSON.parse(result);
-
-            if (json.error) {
-                $.post("./ajax.php", { call: "insertText", message: message, isQuestion: isQuestion }, result => {
-
-                    //console.log(result);
-
-                });
-
-                callback("Hmm, I don't possess the knowledge to answer that question.");
-            } else {
-                $.post("./ajax.php", { call: "insertTextWithResponse", message: message, isQuestion: isQuestion, response: json.result, isWolframResponse: 1 }, result => {
-
-                    //console.log(result);
-
-                });
-
-                callback(json.result);
+            // Wolfram Alpha Api Inclusion
+            let question = message
+            while (question.includes(" ")) {
+                question = question.replace(" ", "+");
+            }
+            while (question.includes("?")) {
+                question = question.replace("?", "%3f")
             }
 
-        });
+            $.post("./ajax.php", { call: "wolframApiCall", question }, result => {
 
-    } else if (message.includes("good") || message.includes("nice") || message.includes("excellent")) {
-        callback("That good to hear. Do you have any more questions");
-    } else if (message.includes("bad") || message.includes("not nice") || message.includes("tragic")) {
-        callback("That sad to hear. Do you have any more questions");
-    } else {
-        callback("tragic");
-    }
+                let json = JSON.parse(result);
+
+                if (json.error) {
+                    $.post("./ajax.php", { call: "insertText", message: message, isQuestion: isQuestion }, result => {
+
+                        //console.log(result);
+
+                    });
+
+                    callback("Hmm, I don't possess the knowledge to answer that question.");
+                } else {
+                    $.post("./ajax.php", { call: "insertTextWithResponse", message: message, isQuestion: isQuestion, response: json.result, isWolframResponse: 1 }, result => {
+
+                        //console.log(result);
+
+                    });
+
+                    callback(json.result);
+                }
+
+            });
+
+        } else if (isQuestion == 2) {
+            callback("I couldn't understand if that was a question or not. My creator obviously did a very good job in making me, so the problem is you!");
+            $.post("./ajax.php", { call: "insertText", message: message, isQuestion: isQuestion }, result => {
+
+                //console.log(result);
+
+            });
+        }
+         else if (message.includes("good") || message.includes("nice") || message.includes("excellent")) {
+            callback("That good to hear. Do you have any more questions");
+        } else if (message.includes("bad") || message.includes("not nice") || message.includes("tragic")) {
+            callback("That sad to hear. Do you have any more questions");
+        } else {
+            callback("tragic");
+        }
+    });
 }
 
 function respondToBoi(message, callBack2) {
